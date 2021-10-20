061dde8a1b3b31791375aa04930dad37c34f528a
Abhishek Singh <nikku.net@gmail.com>
2021-10-20 20:08:10 +0530

    updated v1.1.0

--- react-social/src/App.js
+++ react-social/src/App.js
@@ -4,24 +4,29 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link, Redirect
 } from "react-router-dom";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register"
 import Profile from "./pages/profile/Profile"
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+
+  const {user} = useContext(AuthContext)
+
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Home/>
+          {user ? <Home/>: <Register/> }
         </Route>
         <Route path="/login">
-          <Login/>
+          { user ? <Redirect to="/" /> : <Login/>}
         </Route>
         <Route path="/register">
-          <Register/>
+        { user ? <Redirect to="/" /> : <Register/>}
         </Route>
         <Route path="/profile/:username">
           <Profile/>
--- react-social/src/pages/login/Login.jsx
+++ react-social/src/pages/login/Login.jsx
@@ -1,10 +1,21 @@
 import "./login.css"
+import {useContext, useRef} from "react"
+import {loginCall} from "../../apiCalls"
+import { AuthContext } from "../../context/AuthContext";
+import {CircularProgress} from "@material-ui/core"
 
 function Login() {
 
-    const handleClick = ()=>{
-        
+    const email = useRef();
+    const password = useRef();
+    const {user, isFetching, error, dispatch} = useContext(AuthContext)
+
+    const handleClick = (e)=>{
+        e.preventDefault()
+        loginCall({email: email.current.value, password: password.current.value},dispatch)
     }
+
+    console.log(user)
 
     return (
         <div>
@@ -16,11 +27,11 @@ function Login() {
                     </div>
                     <div className="loginRight" onSubmit={handleClick}>
                         <form className="loginBox">
-                            <input placeholder="Email" type="email" className="loginInput" />
-                            <input placeholder="Password" type="password" className="loginInput" />
-                            <button className="loginButton">Log In</button>
+                            <input placeholder="Email" type="email" required ref={email} className="loginInput" />
+                            <input placeholder="Password" type="password" required ref={password} minLength="6" className="loginInput" />
+                            <button className="loginButton" type="submit" disabled={isFetching} >{ isFetching ? <CircularProgress color="white" size="20px" /> : "Log In"}</button>
                             <span className="loginForgot">Forgot Password?</span>
-                            <button className="loginRegisterButton">Create Account</button>
+                            <button className="loginRegisterButton">{ isFetching ? <CircularProgress color="white" size="20px" /> : "Create Account"}</button>
                         </form>
                     </div>
                 </div>
